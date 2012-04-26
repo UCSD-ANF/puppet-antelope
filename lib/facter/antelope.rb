e079527d96e438e4aa3ff8973833e028e2b8125c
Geoff Davis <gadavis@ucsd.edu>
2012-04-25 19:08:21 -0700

    Add latest_antelope_version fact

--- lib/facter/antelope.rb
+++ lib/facter/antelope.rb
@@ -1,4 +1,17 @@
+# Fact: antelope_versions
+# Fact: latest_antelope_version
+#
+# These two facts represent the installed versions of BRTT Antelope on
+# a system. It determines this by looking at /opt/antelope for
+# directories matching the Antelope version naming convention, and then
+# checks to see if setup.sh is present in the directory.
+#
+# The lastest_antelope_version is the most recent version of Antelope
+# installed on the system.
+
+# Antelope always lives under this directory
 basedir = '/opt/antelope'
+
 Facter.add(:antelope_versions) do
   confine :kernel => %w{Linux SunOS Darwin}
   setcode do
@@ -10,8 +23,19 @@ Facter.add(:antelope_versions) do
       next unless dir =~ /^\d+\.\d+(-64)?p?/
       next unless File.exists?(File.join(basedir, dir, 'setup.sh'))
       versions.insert(-1,dir)
-
     end
+
     versions.join(',')
+  end
+end
+
+Facter.add(:latest_antelope_version) do
+  confine :kernel => %w{Linux SunOS Darwin}
+  setcode do
+    version=nil
+    versionstr = Facter.value('antelope_versions')
+    version = versionstr.split(',').last unless versionstr == nil
+
+    version
   end
 end
