b937d0211fd0afca801eb2d82fbb2bfe90c7c07a
Geoff Davis <gadavis@ucsd.edu>
2012-04-27 16:49:32 -0700

    Fix facts when Antelope is not installed

--- lib/facter/antelope_latest_perl.rb
+++ lib/facter/antelope_latest_perl.rb
@@ -3,23 +3,26 @@
 # The path to the highest version of Perl as distributed by BRTT
 #
 
-Facter.add(:antelope_latest_perl) do
-  # Antelope always lives under this directory
-  basedir = '/opt/antelope'
+# Antelope always lives under this directory
+basedir = '/opt/antelope'
 
-  confine :kernel => %w{Linux SunOS Darwin}
+if File.directory?(basedir) || File.symlink?(basedir)
+  Facter.add(:antelope_latest_perl) do
 
-  setcode do
-    result = nil
-    dirs=Dir.entries(basedir).sort
+    confine :kernel => %w{Linux SunOS Darwin}
 
-    dirs.each do |dir|
-      dir=dir.chomp
-      next unless dir =~/^perl\d+\.\d+(.\d+)?(-64)?$/
-      next unless File.exists?(File.join(basedir, dir, 'bin/perl'))
-      result = dir
+    setcode do
+      result = nil
+      dirs=Dir.entries(basedir).sort
+
+      dirs.each do |dir|
+        dir=dir.chomp
+        next unless dir =~/^perl\d+\.\d+(.\d+)?(-64)?$/
+        next unless File.exists?(File.join(basedir, dir, 'bin/perl'))
+        result = dir
+      end
+
+      result
     end
-
-    result
   end
 end
--- lib/facter/antelope_versions.rb
+++ lib/facter/antelope_versions.rb
@@ -6,24 +6,29 @@
 # present in the directory.
 #
 
-Facter.add(:antelope_versions) do
-  # Antelope always lives under this directory
-  basedir = '/opt/antelope'
+# Antelope always lives under this directory
+basedir = '/opt/antelope'
 
-  confine :kernel => %w{Linux SunOS Darwin}
+if File.directory?(basedir) || File.symlink?(basedir)
 
-  setcode do
-    versions = Array.new
-    dirs=Dir.entries(basedir).sort
+  Facter.add(:antelope_versions) do
 
-    dirs.each do |dir|
-      dir=dir.chomp
-      next unless dir =~ /^\d+\.\d+(-64)?p?/
-      next unless File.exists?(File.join(basedir, dir, 'setup.sh'))
-      versions.insert(-1,dir)
+    confine :kernel => %w{Linux SunOS Darwin}
+
+    setcode do
+      versions = Array.new
+      dirs=Dir.entries(basedir).sort
+
+      dirs.each do |dir|
+        dir=dir.chomp
+        next unless dir =~ /^\d+\.\d+(-64)?p?/
+        next unless File.exists?(File.join(basedir, dir, 'setup.sh'))
+        versions.insert(-1,dir)
+      end
+
+      versions.join(',')
     end
 
-    versions.join(',')
   end
 
 end
