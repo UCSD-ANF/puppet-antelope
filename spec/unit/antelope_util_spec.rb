4d412786380fafaafae62b2648fe9b8f6eb8f88a
Geoff Davis <gadavis@ucsd.edu>
2015-01-08 13:49:39 -0800

    Add more tests for sort_versions

--- spec/unit/antelope_util_spec.rb
+++ spec/unit/antelope_util_spec.rb
@@ -10,10 +10,30 @@ describe Facter::Util::Antelope do
   describe '.sort_versions' do
     subject(:data) { described_class.sort_versions(a,b) }
 
-    context 'equal versions' do
+    context 'equal 64-bit versions' do
       let(:a) { '5.2-64' }
       let(:b) { '5.2-64' }
       it { should eq 0 }
+    end
+    context 'equal unadorned versions' do
+      let(:a) { '5.3' }
+      let(:b) { '5.3' }
+      it { should eq(0) }
+    end
+    context 'equal pre versions' do
+      let(:a) { '5.3pre' }
+      let(:b) { '5.3pre' }
+      it { should eq(0) }
+    end
+    context 'equal post versions' do
+      let(:a) { '5.3post' }
+      let(:b) { '5.3post' }
+      it { should eq(0) }
+    end
+    context 'equal p versions' do
+      let(:a) { '4.11p' }
+      let(:b) { '4.11p' }
+      it { should eq(0) }
     end
     context '5.2-64 and 5.2-64p' do
       let(:a) { '5.2-64' }
@@ -39,6 +59,11 @@ describe Facter::Util::Antelope do
       let(:a) { '5.3post' }
       let(:b) { '5.4pre' }
       it { should eq(-1) }
+    end
+    context '5.4pre and 5.3post' do
+      let(:a) { '5.4pre' }
+      let(:b) { '5.3post' }
+      it { should eq(1) }
     end
   end
 
