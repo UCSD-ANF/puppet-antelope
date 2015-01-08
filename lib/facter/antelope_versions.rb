0accbe6290ff62b50435602e0546afb33936a600
Geoff Davis <gadavis@ucsd.edu>
2015-01-08 13:15:32 -0800

    Add spec for antelope_versions
    Fixed up a bug with prior class simplifications.

--- lib/facter/antelope_versions.rb
+++ lib/facter/antelope_versions.rb
@@ -9,16 +9,17 @@
 require 'facter/util/antelope'
 module Facter::Antelope::Versions
   def self.add_facts
-    confine :kernel => %w{Linux SunOS Darwin}
-    versions = Facter::Util::Antelope.get_versions
-    Facter.add(:antelope_versions) do
-      setcode { versions.join(',') } unless versions.nil?
-    end
 
-    Facter.add(:antelope_versions_array) do
-      setcode { versions } unless versions.nil?
+    if %w{Linux SunOS Darwin}.include? Facter.value(:kernel)
+      versions = Facter::Util::Antelope.get_versions
+      Facter.add(:antelope_versions) do
+        setcode { versions.join(',') } unless versions.nil?
+      end
+      Facter.add(:antelope_versions_array) do
+        setcode { versions } unless versions.nil?
+      end
     end
   end
 end
-Facter.Antelope.Versions.add_facts
+Facter::Antelope::Versions.add_facts
 
