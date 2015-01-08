1461437f32c8d727cfd6776ac20469910fe678df
Geoff Davis <gadavis@ucsd.edu>
2015-01-08 11:49:43 -0800

    Add spec for antelope_contrib_basedir fact

---
+++ spec/unit/antelope_contrib_basedir_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'facter/antelope_contrib_basedir'
+require 'facter/util/antelope'
+
+describe 'antelope_contrib_basedir fact', :type => :fact do
+  let(:fact) { Facter.fact(:antelope_contrib_basedir) }
+  subject(:antelope_contrib_basedir) { fact.value }
+
+  before :each do
+    Facter::Util::Antelope.should_receive(:get_versions).and_return([
+      '5.4', '5.4post'])
+    File.should_receive('directory?').with(
+      '/opt/antelope/5.4/contrib/bin').and_return(false)
+    File.should_receive('directory?').with(
+      '/opt/antelope/5.4post/contrib/bin').and_return(true)
+    Facter::Antelope::ContribFact.add_facts
+  end
+
+  after :each do
+    # Make sure we're clearing out Facter every time
+    Facter.clear
+    Facter.clear_messages
+  end
+
+  it { should_not be_nil }
+  it { should eql({
+    '5.4' => '',
+    '5.4post' => '/contrib', })
+  }
+end
+
