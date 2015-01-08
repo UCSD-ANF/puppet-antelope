7a973d9e24112a676500aff4f9a02890920e7df2
Geoff Davis <gadavis@ucsd.edu>
2015-01-08 11:43:52 -0800

    refactor antelope_versions
    Use better namespacing.
    Only load Facter::Util::Antelope.get_versions once for both facts

--- lib/facter/antelope_versions.rb
+++ lib/facter/antelope_versions.rb
@@ -7,30 +7,18 @@
 #
 
 require 'facter/util/antelope'
+module Facter::Antelope::Versions
+  def self.add_facts
+    confine :kernel => %w{Linux SunOS Darwin}
+    versions = Facter::Util::Antelope.get_versions
+    Facter.add(:antelope_versions) do
+      setcode { versions.join(',') } unless versions.nil?
+    end
 
-Facter.add(:antelope_versions) do
-
-  confine :kernel => %w{Linux SunOS Darwin}
-
-  versions = Facter::Util::Antelope.get_versions
-  unless versions.nil?
-    setcode do
-      return versions.join(',')
+    Facter.add(:antelope_versions_array) do
+      setcode { versions } unless versions.nil?
     end
   end
-
 end
-
-Facter.add(:antelope_versions_array) do
-
-  confine :kernel => %w{Linux SunOS Darwin}
-
-  versions = Facter::Util::Antelope.get_versions
-  unless versions.nil?
-    setcode do
-      return versions
-    end
-  end
-
-end
+Facter.Antelope.Versions.add_facts
 
