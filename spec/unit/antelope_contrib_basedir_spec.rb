652a7964a2ea9f9b86a4e7af1d29a90624e0e794
Geoff Davis <geoff@geoffdavis.com>
2020-06-04 12:51:48 -0700

    Refactor with shared examples
    Also expand skip notice

--- spec/unit/antelope_contrib_basedir_spec.rb
+++ spec/unit/antelope_contrib_basedir_spec.rb
@@ -6,7 +6,8 @@ require 'facter/antelope_contrib_basedir'
 require 'byebug'
 
 describe 'Antelope Contrib Basedir Specs' do
-  let(:version_dirs) { { '5.2-64' => false, '5.4' => false, '5.4post' => true } }
+  VERSION_DIRS = { '5.2-64' => false, '5.4' => false, '5.4post' => true }.freeze
+  let(:version_dirs) { VERSION_DIRS }
   let(:expected_contrib_basedir) do
     {
       '5.2-64' => '',
@@ -15,40 +16,55 @@ describe 'Antelope Contrib Basedir Specs' do
     }
   end
 
+  shared_context 'mocked contrib_subdir_exists?' do
+    before(:each) do
+      Facter.clear
+      allow(Facter::Util::Antelope).to receive(:versions).and_return(version_dirs.keys).at_least(:once)
+      version_dirs.each do |version, dir_exists|
+        allow(Facter::Antelope::Contrib).to receive(:contrib_subdir_exists?)\
+          .with(version).at_least(:once).and_return(dir_exists)
+      end
+      Facter::Antelope::Contrib.add_facts
+    end
+  end
+
   describe Facter::Antelope::Contrib do
-    context 'with versions == 5.2-64, 5.4, 5.4post' do
-      before(:each) do
-        allow(Facter::Util::Antelope).to receive(:versions).and_return(version_dirs.keys)
-        version_dirs.each do |version, dir_exists|
-          allow(File).to receive(:directory?)\
-            .with("/opt/antelope/#{version}/contrib/bin")\
-            .and_return(dir_exists)
+    describe 'contrib_subdir_exists?' do
+      VERSION_DIRS.each do |version, dir_exists|
+        context "with version #{version} and dir_exists #{dir_exists}" do
+          it do
+            expect(File).to receive(:directory?)\
+              .with("/opt/antelope/#{version}/contrib/bin")\
+              .and_return(dir_exists)
+            expect(described_class.contrib_subdir_exists?(version)).to eq dir_exists
+          end
         end
       end
+    end
+    describe 'contrib_dirs' do
+      context "with versions #{VERSION_DIRS.keys.join(', ')}" do
+        include_context 'mocked contrib_subdir_exists?'
 
-      it 'returns the expected contrib basedirs' do
-        expect(described_class.contrib_dirs).to eq(expected_contrib_basedir)
+        it 'returns the expected contrib basedirs' do
+          expect(described_class.contrib_dirs).to eq(expected_contrib_basedir)
+        end
       end
     end
   end
 
-  describe 'antelope_contrib_basedir', type: :fact do
-    before(:each) { Facter.clear }
+  describe 'antelope_contrib_basedir fact', type: :fact do
+    subject(:antelope_contrib_basedir) { fact.value }
+
+    let(:fact) { Facter.fact(:antelope_contrib_basedir) }
+
     after(:each) { Facter.clear }
 
-    context 'when versions == 5.2-64, 5.4, 5.4post' do
-      before(:each) do
-        allow(Facter::Util::Antelope).to receive(:versions).and_return(version_dirs.keys).at_least(:once)
-        version_dirs.each do |version, dir_exists|
-          allow(Facter::Antelope::Contrib).to receive(:contrib_subdir_exists?)\
-            .with(version).at_least(:once).and_return(dir_exists)
-        end
-        Facter::Antelope::Contrib.add_facts
-      end
+    context "with versions #{VERSION_DIRS.keys.join(', ')}" do
+      include_context 'mocked contrib_subdir_exists?'
 
       it 'returns expected contrib basedirs' do
-        skip("facter fact testing doesn't work in PDK 1.1.18")
-        expect(Facter.fact(:antelope_contrib_basedir).value).to eq(expected_contrib_basedir)
+        skip("In test harness, Facter.fact hash.value with version numbers as keys is broken")
+        is_expected.to eq(expected_contrib_basedir)
       end
     end
   end
