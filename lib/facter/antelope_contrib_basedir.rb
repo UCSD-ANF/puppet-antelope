f2bb5e264cf059c23f581aa7a3410545c53d3ef5
Geoff Davis <geoff@geoffdavis.com>
2020-06-04 10:44:03 -0700

    Refactor to add back the add_facts method

--- lib/facter/antelope_contrib_basedir.rb
+++ lib/facter/antelope_contrib_basedir.rb
@@ -3,30 +3,39 @@
 # Fact: antelope_contribbasedir
 require 'facter/util/antelope'
 
-module Facter::Util::Contrib
-  # @summary
-  #   A facter fact to determine the location of the Antelope Contributed Software.
-  #   This varies between older versions of Antelope and newer ones.
-  class << self
-    def contrib_subdir_exists?(version)
-      File.directory?("/opt/antelope/#{version}/contrib/bin")
-    end
-
-    # Determines the contrib directories for all installed versions of Antelope.
-    def contrib_dirs
-      result = {}
-      Facter::Util::Antelope.versions.each do |version|
-        result[version] = if contrib_subdir_exists?(version)
-                            '/contrib'
-                          else
-                            ''
-                          end
+module Facter::Antelope
+  module Contrib
+    # @summary
+    #   A facter fact to determine the location of the Antelope Contributed Software.
+    #   This varies between older versions of Antelope and newer ones.
+    class << self
+      def contrib_subdir_exists?(version)
+        File.directory?("/opt/antelope/#{version}/contrib/bin")
       end
-      result
+
+      # Determines the contrib directories for all installed versions of Antelope.
+      def contrib_dirs
+        result = {}
+        Facter::Util::Antelope.versions.each do |version|
+          result[version] = if contrib_subdir_exists?(version)
+                              '/contrib'
+                            else
+                              ''
+                            end
+        end
+        result
+      end
+
+      def add_facts
+        Facter.add(:antelope_contrib_basedir) do
+          #confine kernel: Facter::Util::Antelope::VALID_KERNELS
+          setcode do
+            contrib_dirs
+          end
+        end
+      end
     end
   end
 end
 
-Facter.add(:antelope_contrib_basedir) do
-  setcode { Facter::Util::Contrib.contrib_dirs }
-end
+Facter::Antelope::Contrib.add_facts
