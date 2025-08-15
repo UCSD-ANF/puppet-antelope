f69eedcfb341afb7332bf089dbe2a153bc6065fa
Geoff Davis <gadavis@ucsd.edu>
2025-08-15 07:47:26 -0700

    refactor(version): integrate shared utility into Puppet function and Facter utility
    Refactor both lib/facter/util/antelope.rb and lib/puppet/functions/antelope/version_compare.rb
    to use the shared version comparison logic from lib/antelope/version_utils.rb.
    Fixes critical bit suffix comparison bugs and ensures consistent version ordering
    across all components.

--- lib/facter/util/antelope.rb
+++ lib/facter/util/antelope.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../../antelope/version_utils'
+
 ##
 ## antelope.rb
 ##
@@ -12,9 +14,12 @@
 # @summary
 #   Utility functions for working with Antelope
 module Facter::Util::Antelope
+  extend Antelope::VersionUtils
+
   VALID_KERNELS = ['Linux', 'SunOS', 'Darwin'].freeze
   ANTELOPE_BASEDIR = '/opt/antelope'
-  RE_VERSION = %r{^(\d+)\.(\d+)(-64)?(pre|post|p)?$}
+  # Use the shared regex from the utility module
+  RE_VERSION = Antelope::VersionUtils::RE_VERSION
 
   # Return a list of all Antelope versions installed on this system
   def self.versions
@@ -28,7 +33,7 @@ module Facter::Util::Antelope
       versions.insert(-1, dir)
     end
 
-    versions.sort { |a, b| sort_versions(a, b) }
+    sort_antelope_versions(versions)
   rescue StandardError
     nil
   end
@@ -42,48 +47,8 @@ module Facter::Util::Antelope
 
   # Sort Antelope versions from oldest to newest
   # 5.2-64 < 5.2-64p < 5.3pre < 5.3 < 5.3post
+  # This method is kept for backward compatibility but now uses the shared comparison logic
   def self.sort_versions(a, b)
-    amatch = RE_VERSION.match(a)
-    bmatch = RE_VERSION.match(b)
-
-    if Integer(amatch[1]) < Integer(bmatch[1])
-      return -1
-    end
-    if Integer(amatch[1]) > Integer(bmatch[1])
-      return 1
-    end
-
-    # major is equal
-    if Integer(amatch[2]) < Integer(bmatch[2])
-      return -1
-    end
-    if Integer(amatch[2]) > Integer(bmatch[2])
-      return 1
-    end
-
-    # major and minor are equal
-    # check the bits
-    if amatch[3].nil? && !bmatch[3].nil?
-      return 1
-    end
-    if bmatch[3].nil? && !amatch[3].nil?
-      return +1
-    end
-
-    # Major, minor, and bits are equal
-    # Check the suffix for pre release versus post-release
-    if amatch[4] == bmatch[4]
-      return 0
-    end
-    if amatch[4] == 'pre'
-      return -1
-    end
-    if bmatch[4] == 'pre'
-      return 1
-    end
-    if (amatch[4] == 'p') || (amatch[4] == 'post')
-      return 1
-    end
-    -1
+    compare_antelope_versions(a, b)
   end
 end
